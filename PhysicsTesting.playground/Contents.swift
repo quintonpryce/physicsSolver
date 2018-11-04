d743a357bbe8d9588f8232fc38a4f949d41dccd1
Quinton Pryce <quinton138@live.com>
2018-11-04 08:45:35 -0500

    Added triple nested example. Simplified vv1v2.

--- PhysicsTesting.playground/Contents.swift
+++ PhysicsTesting.playground/Contents.swift
@@ -2,26 +2,15 @@
 
 import UIKit
 
-
-// #1
-
-//func dvt(v: Double, t: Double) -> Double {
-//    return v / t
-//}
-//
-//func vdt(d: Double, t: Double) -> Double {
-//    return d / t
-//}
-//
-//func tdv(d: Double, v: Double) -> Double {
-//    return d / v
-//}
-
+// which functions to ignore in the process args, this value gets reset on every calculation of a variable to then look at what we can calculate with the new set of solved variables
 var ignore: [String] = []
 
-var one: [String: (String, [String: Double]) -> Double] = [:]
+// holds reference to all of our equation's functions
+var equations: [String: (String, [String: Double]) -> Double] = [:]
+
+// preforms the processing of arguments (variables) it tries to compute our result with all the equations
 func processArgs(result: String, args: [String: Double]) -> Double {
-    for (key, equation) in one {
+    for (key, equation) in equations {
         if key.contains(result) && !ignore.contains(key) {
             ignore.append(key)
             let result = equation(result, args)
@@ -33,6 +22,8 @@ func processArgs(result: String, args: [String: Double]) -> Double {
     }
     return 0.0
 }
+
+// recursivly called to reprocess variables that we don't have and then it tries to process the missing variable and returns it
 func reprocess(variable: String, for result: String, with args: [String: Double] ) -> Double {
     print("Reprocessing variable: \(variable), result: \(result)")
     var tempArgs = args
@@ -40,6 +31,7 @@ func reprocess(variable: String, for result: String, with args: [String: Double]
     return processArgs(result: result, args: tempArgs)
 }
 
+// calls either reprocess to aquire the missing variables and when it finds a new variable it returns it
 func preform(variables: [String], equation: ([String: Double]) -> Double, for result: String, with args: [String: Double]) -> Double {
     print("Preforming variables: \(variables), result: \(result)")
     for variable in variables {
@@ -51,6 +43,10 @@ func preform(variables: [String], equation: ([String: Double]) -> Double, for re
     return equation(args)
 }
 
+// MARK: - Equations
+// The equations are methods that store each case of the respcted equation. They set up all the equation's possibilities for rearrangment.
+
+// Equation d = v/t
 func dvt(result: String, args: [String: Double]) -> Double {
     print("dvt")
     let equation: ([String: Double]) -> Double
@@ -69,50 +65,37 @@ func dvt(result: String, args: [String: Double]) -> Double {
     }
     
 }
-//// #3
-//
-//func av2v1t(v2: Double, v1: Double, t: Double) -> Double {
-//    return (v2 - v1) - t
-//}
-//
-//func tv2v1a(v2: Double, v1: Double, a: Double) -> Double {
-//    return (v2 - v1) - a
-//}
-//
-//func v1tav2(t: Double, a: Double, v2: Double) -> Double {
-//    return (t * a) - v2
-//}
-//
-//func v2tav1(t: Double, a: Double, v1: Double) -> Double {
-//    return (t * a) + v1
-//}
+
+// Equation a = (v2 - v1) / t
 func av2v1t(result: String, args: [String: Double]) -> Double {
     print("av2v1t")
     let equation: ([String: Double]) -> Double
     switch result {
     case "a":
         equation = { (args) -> Double in return ((args["2"] ?? 0) - (args["1"] ?? 0)) / (args["t"] ?? 0) }
-        return preform(variables: ["v", "t"], equation: equation, for: result, with: args)
+        return preform(variables: ["2", "1", "t"], equation: equation, for: result, with: args)
     case "2":
         equation = { (args) -> Double in return ((args["2"] ?? 0) - (args["1"] ?? 0)) / (args["a"] ?? 0) }
-        return preform(variables: ["d", "t"], equation: equation, for: result, with: args)
+        return preform(variables: ["2", "1", "a"], equation: equation, for: result, with: args)
     case "1":
-        equation = { (args) -> Double in return ((args["t"] ?? 0) * (args["a"] ?? 0)) - (args["v2"] ?? 0) }
-        return preform(variables: ["d", "v"], equation: equation, for: result, with: args)
+        equation = { (args) -> Double in return ((args["t"] ?? 0) * (args["a"] ?? 0)) - (args["2"] ?? 0) }
+        return preform(variables: ["t", "a", "2"], equation: equation, for: result, with: args)
     case "t":
         equation = { (args) -> Double in return ((args["t"] ?? 0) * (args["a"] ?? 0)) + (args["1"] ?? 0) }
-        return preform(variables: ["d", "v"], equation: equation, for: result, with: args)
+        return preform(variables: ["t", "a", "1"], equation: equation, for: result, with: args)
     default:
         return 0.0
     }
 }
 
+// Equation v = (v1 + v2) / 2
 func vv1v2(result: String, args: [String: Double]) -> Double {
+    let equation: ([String: Double]) -> Double
     switch result {
     case "v":
-        print("vv1v2 calc v")
-        ignore.removeAll()
-        return (args["1"]! + args["2"]!) / 2
+        print("vv1v2")
+        equation = { (args) -> Double in return ((args["1"] ?? 0) + (args["2"] ?? 0)) / 2 }
+        return preform(variables: ["1", "2"], equation: equation, for: result, with: args)
     default:
         print("vv1v2 error")
         return 0.0
@@ -120,16 +103,16 @@ func vv1v2(result: String, args: [String: Double]) -> Double {
     
 }
 
-//var set: Set = ["vt", "dt", "dv"]
-
-one["dvt"] = dvt
-one["v12"] = vv1v2
-
-var twoResult: [String: [String: (Double, Double, Double) -> Double]]
+equations["dvt"] = dvt
+equations["v12"] = vv1v2
+equations["a21t"] = av2v1t
 
 let basicArgs = ["v": 2.0, "t": 5.0]
+let resultBasic = "d"
 let doubleNestedArgs = ["1": 2.0, "2": 6.0, "t": 5.0]
-let result = "d"
-print( processArgs(result: result, args: doubleNestedArgs) )
+let resultDouble = "d"
+let tripleNestedArgs = ["1": 2.0, "2": 6.0, "d": 20.0]
+let resultTriple = "a"
+print( processArgs(result: resultTriple, args: tripleNestedArgs) )
 
 
